bd71c3c1cc23248ada9318aed01d0a1f69f1d74e
Ekky Hermestian IW <ekkyhiw@gmail.com>
2019-09-01 23:36:26 +0700

    include library

--- deploy/deploy.ino
+++ deploy/deploy.ino
@@ -1,3 +1,18 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+//needed for wifi manager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+#include <ESP8266HTTPClient.h>
+
+#include <ArduinoJson.h>
+
 void setup() {
   // put your setup code here, to run once:
 
